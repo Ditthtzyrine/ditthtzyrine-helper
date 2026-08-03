06141158fe17bf8541c87350fac00ea505dc9d7f
Radithya Atha R <ju625a@gmail.com>
2026-08-04 02:13:19 +0700

    Create formatBytes.js

---
+++ src/formatBytes.js
@@ -0,0 +1,20 @@
+export default function formatBytes(bytes) {
+
+    if (!bytes) return "0 B";
+
+    const sizes = [
+        "B",
+        "KB",
+        "MB",
+        "GB",
+        "TB"
+    ];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+
+    return (
+        (bytes / Math.pow(1024, i)).toFixed(2) +
+        " " +
+        sizes[i]
+    );
+}
