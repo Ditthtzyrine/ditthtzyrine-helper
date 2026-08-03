d950c3a169fd75cafb543c29597af385fb04a87f
Radithya Atha R <ju625a@gmail.com>
2026-08-04 02:12:53 +0700

    Create runtime.js

---
+++ src/runtime.js
@@ -0,0 +1,19 @@
+export default function runtime(seconds) {
+
+    seconds = Number(seconds);
+
+    const d = Math.floor(seconds / 86400);
+
+    const h = Math.floor(seconds % 86400 / 3600);
+
+    const m = Math.floor(seconds % 3600 / 60);
+
+    const s = Math.floor(seconds % 60);
+
+    return [
+        d && `${d}d`,
+        h && `${h}h`,
+        m && `${m}m`,
+        s && `${s}s`
+    ].filter(Boolean).join(" ");
+}
